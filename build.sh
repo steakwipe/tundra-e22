64547d7db3097a6ff40762172d3228e3224a64d7
Ray Miller <steakwipe@gmail.com>
2018-02-19 05:30:14 -0800

    typo fixy fixy, now including built theme

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-edje_cc $@ -id . -fd . default.edc -o ~/.elementary/themes/neptune_remix.edj
+# BE ADVISED
+# This script dumps the compiled theme directly into your ~/.elementary/themes directory.
+# But you weren't just going to blindly run a script from a stranger were you?
+
+edje_cc $@ -id . -fd . default.edc -o ~/.elementary/themes/tundra.edj
